f6442df5ba3e978564db0d88ea038415ed658c89
Mason <masonlynass@gmail.com>
2023-06-23 12:00:45 -0700

    beginning of styling and home page

---
+++ README.md
@@ -0,0 +1,15 @@
+# what we wanna see
+
+Header and footer on all pages
+Link to tickets somewhere on all pages
+
+Home page with poster
+
+Music page with:
+Link, description, name, 
+for all bands
+
+Contact page with email and info about the gig:
+- Dates
+- Location
+- 
---
+++ js/main.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('menu-button');
+const menuUL = document.getElementById('menuUL');
+
+button.addEventListener('click', function(){
+  if (menuUL.classList.contains('is-active')) {
+    menuUL.classList.remove('is-active')
+  } else menuUL.classList.add('is-active');
+});
